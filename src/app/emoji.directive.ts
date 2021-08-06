467b7e6e8cd7f491d15bdf07efbd067097ee632b
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-06 11:43:06 +0530

    Angular-No-Argument-Custom-Directive-Example

---
+++ src/app/emoji.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appEmoji]'
+})
+export class EmojiDirective implements OnInit {
+
+  constructor(private el: ElementRef) {}
+
+   ngOnInit() {
+    this.el.nativeElement.textContent   +='✌️';
+  }
+}
